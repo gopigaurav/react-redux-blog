f8ba8b45973d9ac6de24ea47b379197c350b3088
gopi <gopigaurav9@gmail.com>
2021-05-17 19:14:46 +0530

    Updates

---
+++ precache-manifest.a1970180dc9a7d2e7269ea885475edc1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "970498ae8040292bc26fafbaf1dcb3a1",
+    "url": "/react-redux-blog/index.html"
+  },
+  {
+    "revision": "5cd26133e63be62802cb",
+    "url": "/react-redux-blog/static/css/main.872cd786.chunk.css"
+  },
+  {
+    "revision": "1fdef9df165d9fad444d",
+    "url": "/react-redux-blog/static/js/2.b28662e7.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-redux-blog/static/js/2.b28662e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cd26133e63be62802cb",
+    "url": "/react-redux-blog/static/js/main.30dd2299.chunk.js"
+  },
+  {
+    "revision": "b82bbdc5f2abf6c9b70d",
+    "url": "/react-redux-blog/static/js/runtime-main.eff2aaa2.js"
+  }
+]);
